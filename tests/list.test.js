2f05da20c0c07fce2dfe71a4e813f1411a11b238
Maryna Kapula <maryna.kapula@gmail.com>
2025-12-02 13:12:51 +0200

    4.4 valmis

--- tests/list.test.js
+++ tests/list.test.js
@@ -1,10 +1,41 @@
-const { test } = require('node:test')
+
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
 
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
+
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+  ]
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  const listWithManyBlogs = [
+    { likes: 7 },
+    { likes: 4 },
+    { likes: 2 },
+  ]
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listWithManyBlogs)
+    assert.strictEqual(result, 13)
+  })
 })
