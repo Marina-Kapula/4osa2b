ae16f164fb29598a5ba5ce6fc69b6915c286365b
Maryna Kapula <maryna.kapula@gmail.com>
2025-12-03 12:54:55 +0200

    4.5 valmis

--- tests/list.test.js
+++ tests/list.test.js
@@ -1,11 +1,14 @@
-
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+// 4.3 dummy
+test('dummy returns one', () => {
+  const result = listHelper.dummy([])
+  assert.strictEqual(result, 1)
+})
 
-
-
+// 4.4 totalLikes
 describe('total likes', () => {
   test('of empty list is zero', () => {
     const result = listHelper.totalLikes([])
@@ -37,5 +40,55 @@ describe('total likes', () => {
   test('of a bigger list is calculated right', () => {
     const result = listHelper.totalLikes(listWithManyBlogs)
     assert.strictEqual(result, 13)
+  })
+})
+
+// 4.5 favoriteBlog
+describe('favorite blog', () => {
+  test('of empty list is null', () => {
+    const result = listHelper.favoriteBlog([])
+    assert.strictEqual(result, null)
+  })
+
+  const blogs = [
+    {
+      _id: '1',
+      title: 'Blog one',
+      author: 'Author A',
+      url: 'http://example.com/1',
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: '2',
+      title: 'Blog two',
+      author: 'Author B',
+      url: 'http://example.com/2',
+      likes: 10,
+      __v: 0,
+    },
+    {
+      _id: '3',
+      title: 'Blog three',
+      author: 'Author C',
+      url: 'http://example.com/3',
+      likes: 7,
+      __v: 0,
+    },
+  ]
+
+  test('of a bigger list returns blog with most likes', () => {
+    const result = listHelper.favoriteBlog(blogs)
+
+    const expected = {
+      _id: '2',
+      title: 'Blog two',
+      author: 'Author B',
+      url: 'http://example.com/2',
+      likes: 10,
+      __v: 0,
+    }
+
+    assert.deepStrictEqual(result, expected)
   })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,9 +1,17 @@
-const dummy = (blogs) => {
-  return 1
-}
+// 4.3 dummy
+const dummy = (blogs) => {return 1}
 
-const totalLikes = (blogs) => {
-  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
-}
-module.exports = { dummy, totalLikes }
 
+// 4.4 totalLikes
+const totalLikes = (blogs) => {return blogs.reduce((sum, blog) => sum + blog.likes, 0)}
+
+// 4.5 favoriteBlog
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  return blogs.reduce((fav, blog) =>blog.likes > fav.likes ? blog : fav)
+}
+ // 4.3 4.4 4.5 
+module.exports = {dummy,totalLikes,favoriteBlog,}
